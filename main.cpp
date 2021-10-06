ff0c2401be7701ac1e5d40d097064bc99c754833
roflanpotsan <l1sZ@ya.ru>
2021-10-06 10:36:28 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Lab 2 Var 4" << endl;
+
+    int n;
+    cout << "Input Fibonacci number index: ";
+    cin >> n;
+
+    int fnum = 1, snum = 1, ans;
+
+    if (n == 0 || n == 1) cout << "F(" << n << ") = " << 1 << endl;
+    else
+    {
+        for (int i = 1; i < n; i++)
+        {
+            ans = fnum + snum;
+            fnum = snum;
+            snum = ans;
+        }
+        cout << "F(" << n << ") = " << ans << endl;
+    }
+
+    return 0;
+}
